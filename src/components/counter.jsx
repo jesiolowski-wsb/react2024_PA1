139a349f1cbfd8efa2c9cd886abb86e2c23fa994
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-05-04 11:08:29 +0200

    inline function reference added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,32 +2,19 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
-  const [tagsList, setTagsList] = useState([]);
 
   const formatCount = () => (count === 0 ? "Zero" : count);
   let classes = "badge m-2 badge-";
   classes += count === 0 ? "warning" : "primary";
 
-  const renderTags = () =>
-    tagsList ? (
-      <ul>
-        {tagsList.map((tag) => (
-          <li>{tag}</li>
-        ))}
-      </ul>
-    ) : (
-      <p>There are no tags</p>
-    );
-
-  const handleIncrement = () => {
-    return setCount(count + 1);
+  const handleIncrement = (incrementBy) => {
+    setCount(count + incrementBy);
   };
 
   return (
     <>
-      {renderTags()}
       <span className={classes}>{formatCount()}</span>
-      <button onClick={handleIncrement} className="btn btn-secondary">
+      <button onClick={() => handleIncrement(10)} className="btn btn-secondary">
         increment
       </button>
     </>
