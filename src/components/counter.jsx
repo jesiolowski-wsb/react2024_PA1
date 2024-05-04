2723a1f8d4ff8dce095974ed12f3803d9017aabf
filip.jesiolowski.sp <filip.jesiolowski.sp@lhsystems.com>
2024-05-04 13:31:14 +0200

    added decrement functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Counter = ({ counter, onIncrement, onDelete }) => {
+const Counter = ({ counter, onIncrement, onDelete, onDecrement }) => {
   const formatCount = () => (counter.value === 0 ? "Zero" : counter.value);
   let classes = "badge m-2 badge-";
   classes += counter.value === 0 ? "warning" : "primary";
@@ -10,13 +10,21 @@ const Counter = ({ counter, onIncrement, onDelete }) => {
       <span className={classes}>{formatCount()}</span>
       <button
         onClick={() => onIncrement(counter)}
-        className="btn btn-secondary"
+        className="btn btn-secondary btn-sm"
       >
         +
       </button>
       <button
+        onClick={() => onDecrement(counter)}
+        className="btn btn-secondary btn-sm m-2"
+        disabled={counter.value === 0}
+      >
+        -
+      </button>
+
+      <button
         onClick={() => onDelete(counter.id)}
-        className="btn btn-sm btn-danger m-2"
+        className="btn btn-sm btn-danger"
       >
         Delete
       </button>
